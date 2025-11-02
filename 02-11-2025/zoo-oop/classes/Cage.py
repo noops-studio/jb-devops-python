160ec9f37f800911147fc59f7db236ce759ef72e
beery <beeryba05@gmail.com>
2025-11-02 19:48:47 +0200

    Refactor Cage class to implement add_animal method for better animal management

--- 02-11-2025/zoo-oop/classes/Cage.py
+++ 02-11-2025/zoo-oop/classes/Cage.py
@@ -8,3 +8,11 @@ class Cage:
     def __init__(self, cage_number: str):
         self.CageNumber = cage_number
         self.Animals = []
+        
+        
+    def add_animal(self, animal: Animal) -> bool:
+        # now we will check if there are also preditors and non preditors in the same cage
+        if len(self.Animals) < self.MaxAnimalCapacity and (animal.isPredator and any(a.isPrey for a in self.Animals) or not animal.isPredator):
+            self.Animals.append(animal)
+            return True
+        return False
--- 02-11-2025/zoo-oop/main-zoo.py
+++ 02-11-2025/zoo-oop/main-zoo.py
@@ -1,5 +1,13 @@
 from classes.Animal import Animal
+from classes.Cage import Cage
 
 newAnimal = Animal("Lion", "Simba", 5, True, True, True)
 
 print(f"Animal Name: {newAnimal.name}, Nickname: {newAnimal.Nickname}, Age: {newAnimal.Age}, Is Mammal: {newAnimal.isMammal}, Is Hungry: {newAnimal.IsHungry}, Is Predator: {newAnimal.isPredator}")
+
+newCage = Cage("Cage 1")
+
+
+print(f"Cage Number: {newCage.CageNumber}, Max Capacity: {newCage.MaxAnimalCapacity}, Is Clean: {newCage.IsClean}, Animals in Cage: {len(newCage.Animals)}")
+newCage.Animals.append(newAnimal)
+print(f"Animals in Cage after adding one: {len(newCage.Animals)}")
